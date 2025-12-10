2c57cde846915355a5e139f357f1dea71bac7275
maahul-ups <mehul@underpinservices.org>
2025-12-11 00:01:59 +0530

    fix

--- Assets/Scripts/Fish/Fish.cs
+++ Assets/Scripts/Fish/Fish.cs
@@ -9,25 +9,39 @@ internal class Fish : MonoBehaviour
   internal virtual void Initialize(FishData data)
   {
     transform.position = data.fishPath[0].position;
-    MoveAlongPath(, data.minInterval);
+
+    // Convert Transform[] to Vector3[] for DOPath
+    Vector3[] path = new Vector3[data.fishPath.Length];
+    for (int i = 0; i < data.fishPath.Length; i++)
+    {
+      path[i] = data.fishPath[i].position;
+    }
+
+    MoveAlongPath(path, data.minInterval / 1000f); // convert ms to seconds
   }
 
   internal virtual void MoveAlongPath(Vector3[] path, float duration)
   {
     movementTween?.Kill();
-    movementTween = transform.DOPath(path, duration, PathType.CatmullRom, PathMode.Sidescroller2D, 10, Color.black)
+    movementTween = transform.DOPath(
+            path,
+            duration,
+            PathType.CatmullRom,
+            PathMode.Sidescroller2D,
+            10,
+            Color.clear
+        )
         .SetEase(Ease.Linear)
         .OnComplete(() => FishManager.Instance.DespawnFish(this));
   }
 
   internal virtual void TakeDamage(float damage)
   {
-
+    // To be overridden in child classes
   }
 
   internal virtual void Die()
   {
-    // play effect, reward coins, etc.
     FishManager.Instance.DespawnFish(this);
   }
 
